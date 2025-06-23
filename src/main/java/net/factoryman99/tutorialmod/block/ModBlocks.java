95e6ba9aaa028ab30ed3f21db45d08550e2cb39e
Factoryman99 <jmseig99@gmail.com>
2025-06-22 21:45:30 -0400

    did some changes

---
+++ src/main/java/net/factoryman99/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,24 @@
+package net.factoryman99.tutorialmod.block;
+
+import net.factoryman99.tutorialmod.TutorialMod;
+import net.factoryman99.tutorialmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks(TutorialMod.MOD_ID);
+
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
