6f648ed8a7a5571e48d6e445ae29721ffd645053
Factoryman99 <jmseig99@gmail.com>
2025-07-03 14:02:53 -0400

    Added Chisel

--- src/main/java/net/factoryman99/tutorialmod/item/ModItems.java
+++ src/main/java/net/factoryman99/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.factoryman99.tutorialmod.item;
 
 import net.factoryman99.tutorialmod.TutorialMod;
+import net.factoryman99.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -14,6 +15,9 @@ public class ModItems {
 
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
